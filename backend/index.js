ec75c05495042e09b00e8fbd5f1a3237cdbefa3b
intp44 <kdt910404@gmail.com>
2026-03-30 02:06:40 +0900

    fix backend for vercel

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,6 @@ app.delete('/api/todos/:id', async (req, res) => {
   res.json({ message: '삭제 완료' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 서버 실행 중: http://localhost:${PORT}`));
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => console.log(`🚀 서버 실행 중: http://localhost:${PORT}`));
 module.exports = app;
